d913360e983854a776dd73f1768b339654630821
GAL_son <pancerek1@gmail.com>
2023-10-19 16:26:05 +0200

    Lab2 zad2

--- lab2/app.js
+++ lab2/app.js
@@ -3,5 +3,10 @@ const _ = require('lodash');
 let arr = [1,2,4,6,8,4,4,5];
 console.log(arr);
 
-let result = _.mean(arr);
-console.log(result);
+let mean = _.mean(arr);
+let min = _.min(arr);
+let max = _.max(arr);
+
+console.log("mean = " + mean);
+console.log("mean = " + min);
+console.log("mean = " + max);
