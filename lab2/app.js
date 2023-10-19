70d24ca9cf49d770ec33a67eef04b9bc743212c9
GAL_son <pancerek1@gmail.com>
2023-10-19 16:51:23 +0200

    Lab2 zad3

--- lab2/app.js
+++ lab2/app.js
@@ -1,3 +1,6 @@
+
+// Zad 1,2
+console.log("Zad 1, 2");
 const _ = require('lodash');
 
 let arr = [1,2,4,6,8,4,4,5];
@@ -10,3 +13,42 @@ let max = _.max(arr);
 console.log("mean = " + mean);
 console.log("mean = " + min);
 console.log("mean = " + max);
+
+// Zad 3
+console.log("Zad 3");
+function weighted_average(user) {
+    let gradesWeightedSum = 0;
+    let weightsSum = 0;
+    for(const elem of user.allGrades) {
+        weightsSum += elem.weight * elem.grades.length;
+        gradesWeightedSum += _.sum(elem.grades) * elem.weight;
+    }
+
+    let weightedAverage = gradesWeightedSum / weightsSum;
+
+    console.log(user.name + ' ' + user.surname + ': ' + weightedAverage );
+}
+
+const user = {
+    name: 'Imie',
+    surname: 'Nazwisko',
+    allGrades: [
+        {
+            subjectName: 'name1',
+            grades: [5,4,3,5,2],
+            weight: 3
+        },
+        {
+            subjectName: 'name2',
+            grades: [3, 3.5, 2],
+            weight: 1
+        },
+        {
+            subjectName: 'name3',
+            grades: [4,3,3.5],
+            weight: 5
+        }
+    ]
+}
+
+weighted_average(user);
