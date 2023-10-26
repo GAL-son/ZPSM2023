bfa4a27c111a49ab8736fd6d421c72f6ca06e9d5
GAL_son <pancerek1@gmail.com>
2023-10-26 23:06:45 +0200

    Lab3 zad4

--- lab3/App.tsx
+++ lab3/App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useState } from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -30,55 +30,167 @@ import { TouchableOpacity } from 'react-native';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [textContent, setText] = useState(" ");
+  const [isResult, setIsResult] = useState(false);
+
+  const type = (snum : string) => {
+    if (textContent === "" && (snum in ['0', '.'])) {
+      
+    } else {
+      setText(textContent + snum)
+      setIsResult(false);
+    }
+  }
+
+  const clear = () => {
+    setText('');
+    setIsResult(false);
+  }
+
+  const deleteInput = () => {
+    setText(textContent.substring(0,textContent.length-1));
+    setIsResult(false);
+  }
+
+  const calculate = () => {
+    setIsResult(true);
+    try {
+      const result = eval(textContent);
+      setText(result);
+    } catch(e) {
+      setText('ERROR');
+    }
+  }
+
+  const buttons = [
+    'AC', '', '', '<==',
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+',
+  ];
+
+  const generateButtons = () => {
+    // Generate 
+    return (
+      <View style={styles.buttonContainer}>
+        {buttons.map((button, index) => {
+              const isNumber = !isNaN(parseInt(button)) || button === '.';
+              return (
+<TouchableOpacity
+                      key={index}
+                      style={[
+                        styles.button,
+                        isNumber ? styles.buttonNormal : styles.buttonSpecial
+                      ]}
+                      onPress={() => {
+                        if (button === '=') {
+                          calculate();
+                        } else if (button === 'AC') {
+                          clear();
+                        } else if (button === '<==') {
+                          deleteInput();
+                        } else {
+                          type(button);
+                        }
+                      }}
+                  >
+                    <Text style={styles.buttonText}>{button}</Text>
+                  </TouchableOpacity>
+              );
+            })}
+      </View>
+    );
+  }
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const [hidden, setHidden] = useState(true);
-
-  const buttonPressed = () => {
-    setHidden(!hidden);
-  }
-
-  const buttonText = hidden ? "Pokaż" : "Ukryj";
-
   return (
-    <View style = {styles.container}>
-        <View>
-          <Text style = {styles.text}>Zadanie 2</Text>
-          <TouchableOpacity 
-              onPress={buttonPressed}
-              style = {{alignItems:'center', backgroundColor: '#beda51', padding: 10}}>
-            <Text>{buttonText}</Text>
-          </TouchableOpacity>
-        </View>
-        {!hidden && (
-          <View style={styles.hiddenContent}>
-          <Text>Nazywam się</Text>
-          <Text style = {{fontWeight: 'bold'}}>Bartłomiej Gala</Text>
-        </View>
-        )}                  
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text style={[styles.input,( isResult ? styles.inputResult : styles.inputNormal)]}>{textContent}</Text>
       </View>
-
+      {generateButtons()}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    justifyContent: 'flex-end',
+    backgroundColor: "#dfe0e2",
+  },
+
+  inputContainer: {
+    //backgroundColor: "#555555",
+    padding: 15,
+    height: 'auto',
+    alignItems: 'flex-end',
+  },
+
+  input: {
+    fontSize: 45,
+  },
+  
+  inputNormal: {
+    color: '#607285'
+  },
+
+  inputResult: {
+    color: '#3b28cc'
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+
+  button: {
+    height: 100,
+    width: '25%',
+    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontSize: 25,
+  },
+
+  buttonNormal: {
+    backgroundColor: '#75abbc'
+  },
+
+  buttonSpecial: {
+    backgroundColor: '#3b28cc'
+  },
+
+  fieldHeight: {
+    height: 100,
+  },
+
+  buttonClear: {
+    backgroundColor: '#F35555'
+  },
+
+  buttonDoubleWidth: {
+    width: '50%',
+  },
+  textField: {
+    display: 'flex',
+    width: '100%',
+    backgroundColor: '#555555',
   },
   text: {
-    alignSelf: 'center',
-    margin: 8,
-    fontSize: 30,
+    color: '#dddddd',
+    alignSelf: 'flex-end',
+    margin: 1,
+    fontSize: 65,
   },
-  hiddenContent: {
-    marginTop: 20,
-    alignItems: 'center',
-  }
+
 });
 
 export default App;
