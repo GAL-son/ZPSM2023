a993134a30379f92d3f8d62d6d792ff4845fbb30
GAL_son <pancerek1@gmail.com>
2023-11-02 12:50:02 +0100

    Lab4 added HorizontalView - still not functional

--- lab3/App.tsx
+++ lab3/App.tsx
@@ -7,10 +7,27 @@
 
 import React from 'react';
 import VerricalView from './screens/VerticalView';
+import HorizontalView from './screens/HorizontalView';
+import { Dimensions, View } from 'react-native';
+import { useState } from "react";
 
 function App(): JSX.Element {
+  
+  const isPortrait = () => {
+    const dim = Dimensions.get('screen');
+    return dim.height >= dim.width;
+  };
+
+  const [orientation, setOrientation] = useState(isPortrait() ? 'portrait' : 'landscape');
+
+  Dimensions.addEventListener('change', () => {
+    setOrientation(isPortrait() ? 'portrait' : 'landscape');
+  });
+
   return(
-    <VerricalView/>
+    <>
+      {(orientation==='portrait') ? <VerricalView/> : <HorizontalView/>}
+    </>
   )
 }
 
